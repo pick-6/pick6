7166061ed5a0de060f071c8fb72efdbd2d517d3c
Todd Hendricks <hendricks.ta@gmail.com>
2017-05-08 09:47:27 -0500

    define initial routes to get us started

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,37 @@
 |
 */
 
+// Welcome page
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Logging in and out
+
+Route::get('/login', 'Auth\AuthController@getLogin');
+
+Route::post('/login', 'Auth\AuthController@postLogin');
+
+Route::get('/logout', 'Auth\AuthController@getLogout');
+
+//Registration routes
+
+Route::get('/register', 'Auth\AuthController@getRegister');
+
+Route::post('/register', 'Auth\AuthController@postRegister');
+
+//get requests for account and account information
+
+Route::get('/account/{username}/edit', 'AccountController@edit');
+
+Route::get('/account/{username}/info', 'AccountController@info');
+
+Route::get('/account/{username}', 'AccountController@index');
+
+// Resource routing
+
+Route::resource('bets', 'BetsController');
+
+Route::resource('users', 'UsersController');
+
