75799bc8200cce33a7db03e775e6dbc13f57cb85
Matt Valdez <mattvaldez01@gmail.com>
2017-05-08 09:48:13 -0500

    created homepage using a bootstrap template

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span class="copyright">Have Questions or Feedback? Please <a href="#contact">Contact Us</a> </span>
+            </div>
+            <div class="col-md-12">
+                <span class="copyright">Copyright &copy; Pick 6 2017</span>
+            </div>
+        </div>
+    </div>
+</footer>
