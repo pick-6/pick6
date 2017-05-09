266ffa622a861d98073afaeec4b926c8a7d753fd
Matt Valdez <mattvaldez01@gmail.com>
2017-05-09 11:55:34 -0500

    added views for howtoplay, charities, and contact

---
+++ resources/views/charities.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+@section('content')
+	<section> 
+		<h1>Charities</h1>
+	</section> 
+@stop
---
+++ resources/views/howtoplay.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+@section('content')
+	<section> 
+		<h1>How To Play</h1>
+	</section> 
+@stop
