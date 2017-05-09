7d54c11b9d2d322d68dc32baa42ef93a8283c005
Matt Valdez <mattvaldez01@gmail.com>
2017-05-09 14:35:20 -0500

    styled login and signup pages with inline styling for now

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts.master')
 @section('content')
-	<h1>Logging In</h1>
-
+<section style="background-color: black;padding-bottom: 22%">
+	<h1 style="color: white" class="text-center">Log In</h1>
+<div class="container">
 	<form method="POST" action="{{action('Auth\AuthController@postLogin')}}">
 		{!!csrf_field()!!}
 		<div class="form-group">
 			<label>Email</label> 
-			<input type="email" name="email" value="{{old('email')}}" placeholder="Email" class="form-control">
+			<input style="background-color: #333333; color: #FED136" type="email" name="email" value="{{old('email')}}" placeholder="Email" class="form-control">
 		</div>
 		<div class="form-group">
 			<label>Password</label>
-			<input type="password" name="password" value="{{old('password')}}" placeholder="Password" class="form-control">
+			<input style="background-color: #333333; color: #FED136" type="password" name="password" value="{{old('password')}}" placeholder="Password" class="form-control">
 		</div>
 		<div class="form-group">
-    	    <input type="checkbox" name="remember"> Remember Me
+    	    <input style="background-color: #333333; color: #FED136" type="checkbox" name="remember"> Remember Me
     	</div>
     	<div class="form-group">
     	    <button type="submit" class="btn btn-success">Login</button>
-    	    <a href="/register" class="btn btn-primary pull-right">Go to Sign Up</a>
+    	    <a href="/register" style="color: black" class="btn btn-primary pull-right">Go to Sign Up</a>
     	</div>
 	</form>
-	
+	</div>
+</section>
 @stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,34 +1,42 @@
 @extends('layouts.master')
 
 @section('content')
-<h1>Signing Up</h1>
+<section style="background-color: black">
+<h1 style="color: white" class="text-center">Sign Up</h1>
+<div class="container">
 <form method="POST" action="{{action('Auth\AuthController@postRegister')}}">
     {!! csrf_field() !!}
 
     <div class="form-group">
-        <label>Name</label>
-        <input type="text" name="name" value="{{ old('name') }}" placeholder="Name" class="form-control">
+        <label>First Name</label>
+        <input style="background-color: #333333; color: #FED136" type="text" name="name" value="{{ old('first_name') }}" placeholder="First Name" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label>Last Name</label>
+        <input style="background-color: #333333; color: #FED136" type="text" name="name" value="{{ old('last_name') }}" placeholder="Last Name" class="form-control">
     </div>
 
     <div class="form-group">
         <label>Email</label>
-        <input type="email" name="email" value="{{ old('email') }}" placeholder="Email" class="form-control">
+        <input style="background-color: #333333; color: #FED136" type="email" name="email" value="{{ old('email') }}" placeholder="Email" class="form-control">
     </div>
 
     <div class="form-group">
         <label>Password</label>
-        <input type="password" name="password" placeholder="Password" class="form-control">
+        <input style="background-color: #333333; color: #FED136" type="password" name="password" placeholder="Password" class="form-control">
     </div>
 
     <div class="form-group">
         <label>Confirm Password</label>
-        <input type="password" name="password_confirmation" placeholder="Confirm Password" class="form-control">
+        <input style="background-color: #333333; color: #FED136" type="password" name="password_confirmation" placeholder="Confirm Password" class="form-control">
     </div>
 
     <div class="form-group">
         <button type="submit" class="btn btn-success">Sign Up</button>
-        <a href="/login" class="btn btn-primary pull-right">Go to Login</a>
+        <a href="/login" style="color: black" class="btn btn-primary pull-right">Go to Login</a>
     </div>
 </form>
-	
+</div>
+</section>	
 @stop
