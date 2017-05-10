920f5ac176d8e4d48274578f82c2015e12b952b3
Matt Valdez <mattvaldez01@gmail.com>
2017-05-10 09:41:35 -0500

    started working on account, chartities, and howtoplay pages

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.master')
 @section('content')
-<h1>Account Page</h1>
+<section style="background-color: black">
+	<h1 class="text-center">Welcome, {{Auth::user()->first_name}}</h1>
+	<div class="container text-center">
+	    <a style="color: black" href="/play" class="btn btn-xl">Play Game</a>
+	    <a style="color: black" href="/howtoplay" class="btn btn-xl">How To Play</a>
+	</div>
+</section>
+
+<section style="background-color: black" class="text-center">
+	<h1 class="text-center">Your Account Info</h1>
+	<h3>Full Name: {{Auth::user()->first_name}} {{Auth::user()->last_name}}</h3>
+	<h3>Username: {{Auth::user()->username}}</h3>
+	<h3>Email: {{Auth::user()->email}}</h3>
+</section>
 @stop
