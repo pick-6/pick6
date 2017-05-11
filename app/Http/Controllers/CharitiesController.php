676cd84107df8cdc2bd8f222ef983dd65d6d3527
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-05-11 11:02:56 -0500

    Created charities table on Charities blade. Added pagination for the table in the CharitiesController. Created Resource route for Charities.

--- app/Http/Controllers/CharitiesController.php
+++ app/Http/Controllers/CharitiesController.php
@@ -6,136 +6,141 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Charity;
 
 class CharitiesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
 
-    public function index(Request $request)
-    {
-        if ($request->has('search')) {
-            $posts = Post::join('user_bets', '', '=', 'user.id')
-               ->where('game_id', 'LIKE', '%request->search%')
-               ->orWhere('bet_total', 'LIKE', '%request->search%')
-               ->orderBy('game_id', 'ASC')
-        } else {
-            $posts = Post::orderBy('game_id', 'ASC')->paginate(3);
-        }
+	public function index(Request $request)
+	{
 
-        $data = [];
-        $data['posts'] = $posts;
+		$charities = Charity::orderBy('name')->paginate(25);
 
-        return view('charities.index')->with($data);
-    }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(Request $request)
-    {
-        return view('charities');
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $this->validate($request, Post::$rules);
-        $charities = new Charity();
-        $charities->name = $request->name;
-        $charities->location = $request->location;
-        $charities->category = $request->category;
-        $charities->description = $request->description;
-        $charities->save();
-        Log::info("New charity created", $request->all());
-        $request->session()->flash('successMessage', 'Charity saved successfully');
-        return redirect()->action('CharitiesController@show', [$post->id]);
-    }
+		// if ($request->has('search')) {
+		//     $posts = Post::join('user_bets', '', '=', 'user.id')
+		//        ->where('game_id', 'LIKE', '%request->search%')
+		//        ->orWhere('bet_total', 'LIKE', '%request->search%')
+		//        ->orderBy('game_id', 'ASC')
+		// } else {
+		//     $posts = Post::orderBy('game_id', 'ASC')->paginate(3);
+		// }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $charities = Charity::find($id);
-        if(!$charities) {
-            Log::error("Charity with id of $id not found.");
-            abort(404);
-        }
-        $data = [];
-        $data['charities'] = $charities;
-        return view('charities.show')->with($data);
-    }
+		// $data = [];
+		// $data['charities'] = $charities;
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-       $charities = Charity::find($id);
-        if (!$charities) {
-            Log::error("Charity with id of $id not found.");
-            abort(404);
-        }
-        $data = [];
-        $data['charities'] = $charities;
-        return view('charaties.edit')->with($data);   
-    }
+		return view('charities')->with('charities', $charities);
+	}
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create(Request $request)
+	{
+		return view('charities');
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $this->validate($request, Charity::$rules);
-        $charities = Charity::find($id);
-        if (!$charities) {
-            Log::error("Charity with id of $id not found.");
-            abort(404);
-        }
-        $charities->name = $request->name;
-        $charities->location = $request->location;
-        $charities->category = $request->content;
-        $charities->description = $request->description;
-        $charities->save();
-        $request->session()->flash('successMessage', 'Post updated successfully');
-        return redirect()->action('CharitiesController@show', [$post->id]);   
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$this->validate($request, Post::$rules);
+		$charities = new Charity();
+		$charities->name = $request->name;
+		$charities->location = $request->location;
+		$charities->category = $request->category;
+		$charities->description = $request->description;
+		$charities->save();
+		Log::info("New charity created", $request->all());
+		$request->session()->flash('successMessage', 'Charity saved successfully');
+		return redirect()->action('CharitiesController@show', [$post->id]);
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $charities = Charity::find($id);
-        if (!$charities) {
-            Log::error("charities with id of $id not found.");
-            abort(404);
-        }
-        $charities->delete();
-        $request->session()->flash('successMessage', 'charities deleted successfully');
-        return view('charities.index');
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		$charities = Charity::find($id);
+		if(!$charities) {
+			Log::error("Charity with id of $id not found.");
+			abort(404);
+		}
+		$data = [];
+		$data['charities'] = $charities;
+		return view('charities.show')->with($data);
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+	   $charities = Charity::find($id);
+		if (!$charities) {
+			Log::error("Charity with id of $id not found.");
+			abort(404);
+		}
+		$data = [];
+		$data['charities'] = $charities;
+		return view('charaties.edit')->with($data);   
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$this->validate($request, Charity::$rules);
+		$charities = Charity::find($id);
+		if (!$charities) {
+			Log::error("Charity with id of $id not found.");
+			abort(404);
+		}
+		$charities->name = $request->name;
+		$charities->location = $request->location;
+		$charities->category = $request->content;
+		$charities->description = $request->description;
+		$charities->save();
+		$request->session()->flash('successMessage', 'Post updated successfully');
+		return redirect()->action('CharitiesController@show', [$post->id]);   
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$charities = Charity::find($id);
+		if (!$charities) {
+			Log::error("charities with id of $id not found.");
+			abort(404);
+		}
+		$charities->delete();
+		$request->session()->flash('successMessage', 'charities deleted successfully');
+		return view('charities.index');
+	}
 }
--- resources/views/charities.blade.php
+++ resources/views/charities.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.master')
-@section('content')
-<section style="background-color: black"> 
+@section('content') 
+<section style="background-color: black">	
 	<h1 class="text-center">Charities</h1>
-</section> 
+	<div class="container">
+		<table class="table table-bordered">
+			<tr>
+				<th>Name</th>
+				<th>Wesbite</th>
+				<th>Description</th>
+		</tr>
+				@foreach ($charities as $charity)
+				<tr>
+					<td>{{ $charity->name }}</td>
+					<td><a href="http://{{ $charity->website }}" target="_blank">{{ $charity->website }}</a></td>
+					<td>{{ $charity->description }}</td>
+				</tr>
+			@endforeach
+		</table>
+		{!! $charities->render() !!}
+	</div>
+</section>
 @stop
