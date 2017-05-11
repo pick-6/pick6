0a7e85c51db33c118ae887d16a04ad4061943fd6
Matt Valdez <mattvaldez01@gmail.com>
2017-05-11 10:26:22 -0500

    made navbar more responsive

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -7,7 +7,7 @@
             <div class="intro-text">
                 <div class="intro-lead-in" style="margin-bottom: 10px">Hello, <span>{{ $user->first_name }} {{ $user->last_name }}</span></div>
                 <div class="intro-heading" style="margin-bottom: 0px">Welcome to your account page!</div>
-                <div class="intro-paragraph box_textshadow">Start playing right away or give yourself a refresher on how to play. <p>Additionally, you can find your current account information and may update it if you would like.</div>
+                <div class="intro-paragraph box_textshadow">Start playing right away or give yourself a refresher on how to play. <p>Additionally, you can find your current account information, which you may update if you would like.</div>
                 <div class="intro-heading" style="margin-bottom: 10px">
                 	<a style="color: black" href="/play" class="btn btn-xl">Play Game</a>
 	    			<a style="color: black" href="/howtoplay" class="btn btn-xl">How To Play</a>
