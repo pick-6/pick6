406c193b215d9d2686e103be37e0df0c8ec37c24
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-05-12 09:48:15 -0500

    updated winning selection to correct values

--- database/seeds/GamesSeeder.php
+++ database/seeds/GamesSeeder.php
@@ -24,7 +24,7 @@ class GamesSeeder extends Seeder
         $games2->away = 'Cleveland Browns';
         $games2->home_score = '3';
         $games2->away_score = '10';
-        $games2->winning_selection = '17';
+        $games2->winning_selection = '30';
         $games2->save();
 
         $games3 = new \App\Game();
@@ -32,7 +32,7 @@ class GamesSeeder extends Seeder
         $games3->away = 'Miami Dolphins';
         $games3->home_score = '10';
         $games3->away_score = '17';
-        $games3->winning_selection = '17';
+        $games3->winning_selection = '07';
         $games3->save();
 
         $games4 = new \App\Game();
@@ -40,7 +40,7 @@ class GamesSeeder extends Seeder
         $games4->away = 'Tennessee Titans';
         $games4->home_score = '7';
         $games4->away_score = '14';
-        $games4->winning_selection = '17';
+        $games4->winning_selection = '74';
         $games4->save();
 
         $games5 = new \App\Game();
@@ -48,7 +48,7 @@ class GamesSeeder extends Seeder
         $games5->away = 'Washington Redskins';
         $games5->home_score = '28';
         $games5->away_score = '35';
-        $games5->winning_selection = '17';
+        $games5->winning_selection = '85';
         $games5->save();
 
         $games6 = new \App\Game();
@@ -56,7 +56,7 @@ class GamesSeeder extends Seeder
         $games6->away = 'San Francisco 49ers';
         $games6->home_score = '52';
         $games6->away_score = '10';
-        $games6->winning_selection = '17';
+        $games6->winning_selection = '20';
         $games6->save();
 
         $games7 = new \App\Game();
@@ -64,7 +64,7 @@ class GamesSeeder extends Seeder
         $games7->away = 'Dallas Cowboys';
         $games7->home_score = '35';
         $games7->away_score = '28';
-        $games7->winning_selection = '17';
+        $games7->winning_selection = '58';
         $games7->save();
 
         $games8 = new \App\Game();
@@ -72,7 +72,7 @@ class GamesSeeder extends Seeder
         $games8->away = 'Houston Texans';
         $games8->home_score = '3';
         $games8->away_score = '7';
-        $games8->winning_selection = '17';
+        $games8->winning_selection = '37';
         $games8->save();
 
         $games9 = new \App\Game();
@@ -80,7 +80,7 @@ class GamesSeeder extends Seeder
         $games9->away = 'Detroit Lions';
         $games9->home_score = '21';
         $games9->away_score = '28';
-        $games9->winning_selection = '17';
+        $games9->winning_selection = '18';
         $games9->save();
 
         $games10 = new \App\Game();
@@ -96,7 +96,7 @@ class GamesSeeder extends Seeder
         $games11->away = 'Cincinnati Bengals';
         $games11->home_score = '24';
         $games11->away_score = '12';
-        $games11->winning_selection = '17';
+        $games11->winning_selection = '42';
         $games11->save();
 
         $games12 = new \App\Game();
@@ -104,7 +104,7 @@ class GamesSeeder extends Seeder
         $games12->away = 'St. Louis Rams';
         $games12->home_score = '42';
         $games12->away_score = '49';
-        $games12->winning_selection = '17';
+        $games12->winning_selection = '29';
         $games12->save();
 
         $games13 = new \App\Game();
@@ -112,7 +112,7 @@ class GamesSeeder extends Seeder
         $games13->away = 'Green Bay Packers';
         $games13->home_score = '13';
         $games13->away_score = '17';
-        $games13->winning_selection = '17';
+        $games13->winning_selection = '37';
         $games13->save();
 
         $games14 = new \App\Game();
@@ -120,7 +120,7 @@ class GamesSeeder extends Seeder
         $games14->away = 'Minnesota Vikings';
         $games14->home_score = '25';
         $games14->away_score = '7';
-        $games14->winning_selection = '17';
+        $games14->winning_selection = '57';
         $games14->save();
 
         $games15 = new \App\Game();
@@ -128,7 +128,7 @@ class GamesSeeder extends Seeder
         $games15->away = 'Denver Broncos';
         $games15->home_score = '13';
         $games15->away_score = '7';
-        $games15->winning_selection = '17';
+        $games15->winning_selection = '37';
         $games15->save();
 
     }
