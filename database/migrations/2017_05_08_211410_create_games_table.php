e86339bdbe464aa38ec5cc9b5390d54e856a12d8
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-05-16 15:02:52 -0500

    Added columns to games table in the database

--- database/migrations/2017_05_08_211410_create_games_table.php
+++ database/migrations/2017_05_08_211410_create_games_table.php
@@ -15,11 +15,14 @@ class CreateGamesTable extends Migration
 		Schema::create('games', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->integer('week');
 			$table->string('home');
 			$table->string('away');
 			$table->integer('home_score');
 			$table->integer('away_score');
 			$table->integer('winning_selection');
+			$table->string('winning_charity');
+			$table->string('winning_user');
 		});
 	}
 
