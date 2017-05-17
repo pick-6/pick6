62f80cd6a72d31dff6c1dda359c5e583a1ca3aa9
Matt Valdez <mattvaldez01@gmail.com>
2017-05-16 23:29:08 -0500

    added update function so users can update their account info

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -9,93 +9,64 @@ use App\Http\Controllers\Controller;
 use App\Models\Account;
 use App\Models\Games;
 use App\Models\Selections;
+use App\User;
 
 class AccountController extends Controller
 {
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index()
-	{
-		// return view('account');
-	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create()
-	{
-		//
-	}
+    public function index()
+    {
+        $user = \Auth::user();
+        $games = Games::get();
+        $gamesUserIsPlaying = Selections::where('user_id', "=", $user->id)->get();
+        return view('account')->with(compact('user', 'gamesUserIsPlaying', 'games'));
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request)
-	{
-		//
-	}
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show()
-	{
-		$user = \Auth::user();
-		$games = Games::get();
+    public function create()
+    {
+        abort(404);
+    }
 
-		$gamesUserIsPlaying = Selections::where('user_id', "=", $user->id)->get();
-		
-		return view('account')->with(compact('user', 'gamesUserIsPlaying', 'games'));
-	}
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit($id)
-	{
-		// $user = Post::find($id);
-		// return view('edit')->with('user', $user);
-	}
+    public function store(Request $request)
+    {
+        abort(404);
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, $id)
-	{
-		//
-	}
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    public function show()
+    {
+        abort(404);
+    }
+
+
+    public function edit()
+    {
+        return view('edit');
+    }
+
+
+    public function update(Request $request)
+    {
+        $existingUser = User::find(\Auth::id());
+        $existingUser->first_name = $request->first_name;
+        $existingUser->last_name = $request->last_name;
+        $existingUser->username = $request->username;
+        $existingUser->email = $request->email;
+        $existingUser->password = bcrypt($request->password);
+        $existingUser->save();
+        return redirect()->action('AccountController@index');
+    }
+
+
+    public function destroy($id)
+    {
+        abort(404);
+    }
 }
