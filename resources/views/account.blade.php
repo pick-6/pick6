a3a2649bc4e6b39b9b55c77a3e3afa3f70a1dc43
Matt Valdez <mattvaldez01@gmail.com>
2017-05-17 14:22:10 -0500

    moved Past Game Results dropdown button from playGame to Account page

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -37,6 +37,24 @@
                 @endforeach
             </table>
             <a style="color: black" href="{{action('ResultsController@showGameWinner')}}" class="btn btn-lg">SEE RESULTS</a>
+            
+
+            <!-- OR -->
+            <div class="text-center">
+                <h2 style="color: white;margin-top: 5%">OR</h2>
+            </div>
+
+            <!-- SEE PAST GAME RESULTS -->
+            <div class="dropdown text-center" style="margin-top: 5%">
+                <button class="btn btn-xl dropdown-toggle gameBtn" type="button" data-toggle="dropdown">See Past Game Results <span class="caret"></span></button>
+                <ul class="dropdown-menu scrollable-menu">
+                    @foreach ($games as $game)
+                        @if ($game->date_for_week < date('Y-m-d'))
+                            <li><a class="page-scroll gameSelection" data-id="{{$game->id}}" href="{{action('GamesController@show', [$game->id])}}">Week {{$game->week}} - Game {{$game->id}}: {{$game->home}} vs. {{$game->away}}</a></li>
+                        @endif
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </section>
 
--- resources/views/playGame.blade.php
+++ resources/views/playGame.blade.php
@@ -14,23 +14,6 @@
                 @endforeach
             </ul>
         </div>
-
-        <!-- OR -->
-        <div class="text-center">
-            <h2 style="color: white;margin-top: 5%">OR</h2>
-        </div>
-
-        <!-- SEE PAST GAMES -->
-        <div class="dropdown text-center" style="margin-top: 5%">
-            <button class="btn btn-xl dropdown-toggle gameBtn" type="button" data-toggle="dropdown">See Past Game Results <span class="caret"></span></button>
-            <ul class="dropdown-menu scrollable-menu">
-                @foreach ($games as $game)
-                    @if ($game->date_for_week < date('Y-m-d'))
-                        <li><a class="page-scroll gameSelection" data-id="{{$game->id}}" href="{{action('GamesController@show', [$game->id])}}">Week {{$game->week}} - Game {{$game->id}}: {{$game->home}} vs. {{$game->away}}</a></li>
-                    @endif
-                @endforeach
-            </ul>
-        </div>
     </div>
 </section>
 @stop
