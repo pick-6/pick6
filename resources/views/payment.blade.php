dfeffca52e9ccecd9454c8ef6009ff3506a263bd
Matt Valdez <mattvaldez01@gmail.com>
2017-05-18 16:00:03 -0500

    styled the payment blade more with inline styling for now;

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 @section('content')
 <section>
-	<div class="container">
-		<h1 class="text-center" style="color: white;">Payment Page</h1>
+    <div class="container">
+        <h1 class="text-center" style="color: white;">Payment Page</h1>
 
-		<h2 class="text-center" style="color: white;">Your Picks</h2>
-		    <table class="table table-bordered">
+        <h2 class="text-center" style="color: white;">Your Picks</h2>
+            <table class="table table-bordered">
                 <tr>
                     <th>Game</th>
                     <th>Selection</th>
@@ -20,28 +20,28 @@
                 @endforeach
             </table>
 
-            <div class="container">
-              <form class="form-horizontal" role="form">
-                <fieldset>
-                  <legend style="color: white;">Card Information</legend>
+            <section style="font-family: 'Montserrat', sans-serif;">
+                <form class="form-horizontal" role="form">
+                    <fieldset>
+                  <legend style="color: white;font-weight: bold;" class="text-center">PAYMENT INFORMATION</legend>
                   <div class="form-group">
                     <label class="col-sm-3 control-label" for="card-holder-name" style="color: white;">Name on Card</label>
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" name="card-holder-name" id="card-holder-name" placeholder="Card Holder's Name">
+                    <div class="col-sm-6">
+                      <input style="background-color: #333333;color: #FEC503" type="text" class="form-control" name="card-holder-name" id="card-holder-name" placeholder="Card Holder's Name">
                     </div>
                   </div>
                   <div class="form-group">
                     <label class="col-sm-3 control-label" for="card-number" style="color: white;">Card Number</label>
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" name="card-number" id="card-number" placeholder="Debit/Credit Card Number">
+                    <div class="col-sm-6">
+                      <input style="background-color: #333333;color: #FEC503" type="text" class="form-control" name="card-number" id="card-number" placeholder="Debit/Credit Card Number">
                     </div>
                   </div>
                   <div class="form-group">
                     <label class="col-sm-3 control-label" for="expiry-month" style="color: white;">Expiration Date</label>
                     <div class="col-sm-9">
                       <div class="row">
-                        <div class="col-xs-3">
-                          <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
+                        <div class="col-xs-4">
+                          <select style="background-color: #333333;color: #FEC503" class="form-control col-sm-2" name="expiry-month" id="expiry-month">
                             <option>Month</option>
                             <option value="01">Jan (01)</option>
                             <option value="02">Feb (02)</option>
@@ -57,8 +57,8 @@
                             <option value="12">Dec (12)</option>
                           </select>
                         </div>
-                        <div class="col-xs-3">
-                          <select class="form-control" name="expiry-year">
+                        <div class="col-xs-4">
+                          <select style="background-color: #333333;color: #FEC503" class="form-control" name="expiry-year">
                             <option value="13">2013</option>
                             <option value="14">2014</option>
                             <option value="15">2015</option>
@@ -76,22 +76,30 @@
                     </div>
                   </div>
                   <div class="form-group">
-                    <label class="col-sm-3 control-label" for="cvv" style="color: white;">Card CVV</label>
-                    <div class="col-sm-3">
-                      <input type="text" class="form-control" name="cvv" id="cvv" placeholder="Security Code">
+                    <label class="col-sm-3 control-label" for="cvv" style="color: white;">Card CVV <i class="fa fa-lock" aria-hidden="true"></i></label>
+                        <div class="col-sm-3">
+                            <input style="background-color: #333333;color: #FEC503" type="text" class="form-control" name="cvv" id="cvv" placeholder="Security Code">
+                        </div>
+                        <div class="col-sm-3" style="margin-left: 2%">
+                        <label style="color: white;">
+                            <input style="background-color: #333333;color: #FEC503;margin-top: 10%" type="checkbox"> Remember this card 
+                        </label> 
+                        </div>
                     </div>
-                  </div>
-                  <div class="text-center">
-                    <label style="color: white;">
-                        <input type="checkbox">Remember this card 
-                    </label> 
-                  </div> <br>
+                    <div class="text-center">
+                            <i style="color: white;font-size: 1.5em;" class="fa fa-cc-mastercard" aria-hidden="true"></i>
+                            <i style="color: white;font-size: 1.5em;" class="fa fa-cc-amex" aria-hidden="true"></i>
+                            <i style="color: white;font-size: 1.5em;" class="fa fa-cc-paypal" aria-hidden="true"></i>
+                            <i style="color: white;font-size: 1.5em;" class="fa fa-cc-discover" aria-hidden="true"></i>
+                            <i style="color: white;font-size: 1.5em;" class="fa fa-cc-visa" aria-hidden="true"></i>
+                    </div>
                 </fieldset>
               </form>
-            </div>        
-		<div class="text-center">
-        	<a href="{{action('AccountController@index')}}" class="btn btn-xl dropdown-toggle gameBtn" type="button">Submit Payment</a>
-		</div>
-	</div>
+        <div class="text-center">
+            <a href="{{action('AccountController@index')}}" style="margin-top: 2%" class="btn btn-lg dropdown-toggle gameBtn" type="button">SUBMIT PAYMENT</a>
+        </div>
+            </section>
+
+    </div>
 </section>
 @stop
